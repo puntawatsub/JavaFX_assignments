4d24c7101e37320f605ed056cfff7ec8b84948f0
Puntawat Subhamani <puntawatsub@gmail.com>
2025-09-26 02:11:39 +0300

    Add unsaved changes warning to note editor
    Introduced a warning dialog when closing the note editor with unsaved changes. Refactored note addition logic into a separate method and improved user experience by prompting to save or discard changes before closing.

--- Assignment6_4/src/main/java/controller/NoteEditViewController.java
+++ Assignment6_4/src/main/java/controller/NoteEditViewController.java
@@ -1,13 +1,13 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import model.Note;
 import model.Notebook;
+import view.NoteListView;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class NoteEditViewController {
@@ -19,6 +19,10 @@ public class NoteEditViewController {
 
     @FXML
     private Button editAddButton;
+
+    private Alert alert;
+
+    private boolean recentlyChanged = false;
 
     public void start(Stage stage, Note note) {
 
@@ -33,13 +37,60 @@ public class NoteEditViewController {
             }
         });
 
-        editAddButton.setOnAction(event -> {
-            note.setContent(contentField.getText());
-            note.setTitle(titleField.getText());
-            if (!Arrays.stream(Notebook.getInstance().getNotes()).toList().contains(note)) {
-                Notebook.getInstance().addNote(note);
-            }
-            Notebook.getInstance().writeNotebook();
+        titleField.textProperty().addListener(o -> {
+            recentlyChanged = true;
         });
+
+        contentField.textProperty().addListener(o -> {
+            recentlyChanged = true;
+        });
+
+        editAddButton.setOnAction(event -> {
+            onAdd(note);
+        });
+
+        stage.setOnCloseRequest(event -> {
+            if (recentlyChanged) {
+                event.consume();
+                alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Are you sure?");
+                alert.setContentText("Do you want to add current note?");
+                alert.setHeaderText("Your current changes will not be saved");
+                ButtonType add = new ButtonType("Add");
+                ButtonType doNotAdd = new ButtonType("Do not add");
+                alert.getButtonTypes().removeFirst();
+                alert.getButtonTypes().addAll(add, doNotAdd, ButtonType.CANCEL);
+
+                alert.showAndWait().ifPresent(response -> {
+                    if (response == add) {
+                        onAdd(note);
+                        stage.close();
+                        try {
+                            new NoteListView().loadGUI();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else if (response == doNotAdd) {
+                        stage.close();
+                        try {
+                            new NoteListView().loadGUI();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+
+        });
+    }
+
+    private void onAdd(Note note) {
+        note.setContent(contentField.getText());
+        note.setTitle(titleField.getText());
+        if (!Arrays.stream(Notebook.getInstance().getNotes()).toList().contains(note)) {
+            Notebook.getInstance().addNote(note);
+        }
+        Notebook.getInstance().writeNotebook();
+        recentlyChanged = false;
     }
 }
