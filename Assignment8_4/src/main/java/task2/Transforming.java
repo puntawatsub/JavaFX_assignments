11fc038a25a2de6d2fd48acde939c659a78e263d
Puntawat Subhamani <puntawatsub@gmail.com>
2025-10-11 01:50:05 +0300

    Add IntSum functional interface and use in Transforming
    Introduced a new @FunctionalInterface IntSum for summing two integers. Updated the Transforming class to use IntSum in the reduce operation instead of Integer::sum.

--- Assignment8_4/src/main/java/task2/Transforming.java
+++ Assignment8_4/src/main/java/task2/Transforming.java
@@ -3,10 +3,17 @@ package task2;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@FunctionalInterface
+interface IntSum {
+    int sum(int a, int b);
+}
+
 public class Transforming {
+
     public static void main(String[] args) {
         List<Integer> integerList = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-
-        System.out.println(integerList.stream().filter(i -> i % 2 != 0).map(i -> i * 2).reduce(Integer::sum).get());
+        IntSum sum = (a, b) -> a + b;
+        System.out.println(integerList.stream().filter(i -> i % 2 != 0).map(i -> i * 2).reduce(sum::sum).get());
     }
 }
